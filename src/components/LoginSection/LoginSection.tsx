11daac4d5fee2925f44652b5c37597c1595cf11b
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2024-09-15 20:06:26 +0600

    Update LoginSection.tsx

--- src/components/LoginSection/LoginSection.tsx
+++ src/components/LoginSection/LoginSection.tsx
@@ -10,14 +10,14 @@ const LoginSection = ({ loginLink, signUpLink }: LoginSectionProps) => {
   return (
     <div className='signBox'>
       <div className='signLine'>মন্তব্য করতে লগ ইন করুন অথবা সাইন আপ করুন</div>
-      <div className="gap-y-6">
+      <div className="gap-y-6 flex flex-col md:flex-row">
         <a href={loginLink}>
-          <button className='loginBtn' name='login'>
+          <button className='loginBtn px-6' name='login'>
             লগ ইন
           </button>
         </a>
         <a href={signUpLink}>
-          <button className='signBtn' name='signup'>
+          <button className='signBtn text-nowrap' name='signup'>
             সাইন আপ
           </button>
         </a>
